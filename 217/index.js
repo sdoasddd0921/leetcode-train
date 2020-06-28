759441e54f4409f6c8a3f180409ba2f0c3702cfe
arczhang <arczhang@tencent.com>
2020-06-28 17:23:48 +0800

    feat: pass 217

---
+++ 217/index.js
@@ -0,0 +1,14 @@
+// 存在重复元素
+/**
+ * 给定一个整数数组，判断是否存在重复元素。
+ * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+	const oldLen = nums.length
+	return [...new Set(nums)].length !== oldLen
+};
