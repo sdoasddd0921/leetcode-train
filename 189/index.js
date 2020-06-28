40fa422ed964d3d75e90f1d73b9d89f542422a29
arczhang <arczhang@tencent.com>
2020-06-28 17:03:01 +0800

    feat: 189 更新三次反转法

--- 189/index.js
+++ 189/index.js
@@ -8,11 +8,29 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
-	// 简单的循环
-	for (let i = 0; i < k; i++) {
-		nums.unshift(nums.pop())
+
+// var rotate = function(nums, k) {
+// 	// 简单的循环
+// 	for (let i = 0; i < k; i++) {
+// 		nums.unshift(nums.pop())
+// 	}
+// };
+
+const reverse = (arr, start, end) => {
+	while (end > start) {
+		let [a, b] = [arr[start], arr[end]]
+		arr[start] = b
+		arr[end] = a
+		start++
+		end--
 	}
+}
+var rotate = function(nums, k) {
+	// 三次反转法
+	const end = nums.length - 1
+	reverse(nums, 0, end)
+	reverse(nums, 0, k - 1)
+	reverse(nums, k, end)
 };
 
 // test
