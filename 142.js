dfcf023e9acadc7a99be54b14344a87f450393a4
arczhang <arczhang@tencent.com>
2020-07-01 16:02:58 +0800

    feat: 142 双指针法

--- 142.js
+++ 142.js
@@ -13,11 +13,27 @@
  */
 var detectCycle = function(head) {
 	// hash 法，很慢（9%）
-	const set = new Set()
-	while (head) {
-		if (set.has(head)) return head
-		set.add(head)
-		head = head.next
+	// const set = new Set()
+	// while (head) {
+	// 	if (set.has(head)) return head
+	// 	set.add(head)
+	// 	head = head.next
+	// }
+	// return null
+	// -----------------------
+	// 双指针(56%)
+	let s = f = head
+	while (f) {
+		f = (f.next || {}).next
+		if (!f) return null
+		s = s.next
+		if (s === f) break
 	}
-	return null
+	if (!f) return null
+	f = head
+	while (s !== f) {
+		s = s.next
+		f = f.next
+	}
+	return s
 };
