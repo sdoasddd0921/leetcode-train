ccd485f4bf74ce27e9ee9dca0beb402b6db7cc4e
arczhang <arczhang@tencent.com>
2020-07-01 14:30:45 +0800

    feat: 141 增加更快的方法

--- 141.js
+++ 141.js
@@ -12,12 +12,26 @@
  */
 var hasCycle = function(head) {
 	// 缩减法（22%）
-	if (!head || !head.next) return false
+	// if (!head || !head.next) return false
+	// while (head) {
+	// 	if (head === head.next) return true
+	// 	// 删掉中间的节点，缩小环
+	// 	head.next = (head.next || {}).next
+	// 	head = head.next
+	// }
+	// return false
+};
+var hasCycle = function(head) {
+	// hash 法（97%）
+	const set = new Set()
 	while (head) {
-		if (head === head.next) return true
-		// 删掉中间的节点，缩小环
-		head.next = (head.next || {}).next
-		head = head.next
+		if (!set.has(head)) {
+			// 未出现过
+			set.add(head)
+			head = head.next
+		} else {
+			return true
+		}
 	}
 	return false
 };
