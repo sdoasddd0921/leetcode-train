4a01466fe90af0d9c4c5d80ec64fd26895040f0b
arczhang <arczhang@tencent.com>
2020-07-02 17:33:52 +0800

    feat: 101 递归解法

--- 101.js
+++ 101.js
@@ -13,26 +13,36 @@
 var isSymmetric = function(root) {
 	// 前序遍历和后续遍历应该相等
 	// 2次遍历，巨慢（6%）
-	let l = r = ''
-	function left (node) {
-		if (!node) {
-			l += 'n'
-			return
+	// let l = r = ''
+	// function left (node) {
+	// 	if (!node) {
+	// 		l += 'n'
+	// 		return
+	// 	}
+	// 	l += node.val
+	// 	left(node.left)
+	// 	left(node.right)
+	// }
+	// left(root)
+	// function right (node) {
+	// 	if (!node) {
+	// 		r += 'n'
+	// 		return
+	// 	}
+	// 	r += node.val
+	// 	right(node.right)
+	// 	right(node.left)
+	// }
+	// right(root)
+	// return l === r
+	// 递归，更球慢！？（0%）
+	const loop = (a, b) => {
+		if (a === b) return true
+		if (a && b) {
+			return a.val === b.val && loop(a.left, b.right) && loop(a.right, b.left)
 		}
-		l += node.val
-		left(node.left)
-		left(node.right)
-	}
-	left(root)
-	function right (node) {
-		if (!node) {
-			r += 'n'
-			return
+			return false
 		}
-		r += node.val
-		right(node.right)
-		right(node.left)
-	}
-	right(root)
-	return l === r
+		if (!root) return true
+		return loop(root.left, root.right)
 };
