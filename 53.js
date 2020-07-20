9da082730409be5f30d7de99865eb9dcca998097
arczhang <arczhang@tencent.com>
2020-07-20 17:27:11 +0800

    feat: 53 更快的方法

--- 53.js
+++ 53.js
@@ -5,23 +5,17 @@
  */
 const { max } = Math
 var maxSubArray = function(nums) {
-	// (0%)
+	// (35%%)
 	let len = nums.length
 	let i = 1
 	let pre = big = nums[0]
 	while (i < len) {
 		const cur = nums[i++]
-		pre += cur
-		if (cur > 0) {
-			pre = max(pre, cur)
-			big = max(pre, big)
-		} else {
-			big = max(big, cur)
-		}
-		console.log(big, pre, cur)
+		pre = pre < 0 ? cur : pre + cur
+		if (pre > big) big = pre
+		// console.log(big, pre, cur)
 	}
-	console.log(big)
 	return big
 };
 
-console.log(maxSubArray([-2,-1,-7]))
+console.log(maxSubArray([-2,4,-6,4,-1,2,1,-4,5]))
